daef83646dcdaa0d10dd66a369a6049d0614c0ec
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-07-31 23:38:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,18 @@ client.on('message', msg => {
   }
 });
 
-
+client.on("message", message => {
+    var prefix = "!"
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "skin") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://visage.surgeplay.com/full/256/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
 
 
 
