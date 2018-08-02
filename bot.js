31f068b993e2cdd778185e777f7be55015305911
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-08-02 03:00:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,11 +10,21 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith('-ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms 📶 ")
+                        .addField('**WebSocket:**',api + " ms 📶 ")
+         message.channel.send({embed:embed});
+                        }
+                    });
 
 const codes = {
     ' ': '   ',
