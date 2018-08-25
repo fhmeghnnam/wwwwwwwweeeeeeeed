21d04fefe81feb39eb47df2ff608c8fa69745b5c
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-08-25 15:02:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,21 +36,6 @@ client.on('ready', function(){
 });
 
 
-const client = new Discord.Client();
-
-client.on('message', message => {
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
 
 
 
