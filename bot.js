f4b73ef69b48aa211b018e4ec648f20edf403d54
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-06-03 07:57:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready',async () => {
 378652303881797632
 
 
-    client.channels.find(ch => ch.id === "378652303881797632" && ch.type === 'voice').join();
+    client.channels.find(ch => ch.id === "560897772690210816" && ch.type === 'voice').join();
     clie
   });
 	
