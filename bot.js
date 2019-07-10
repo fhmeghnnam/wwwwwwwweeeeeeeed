73b43c81223fca743ba7457dcda777bfd7538385
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-07-11 00:16:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,24 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-let prefix = "!"
-client.on('ready',async () => {
-  client.channels.find(ch => ch.id === "595626622682398780" && ch.type === 'voice').join();
+const Discord = require('discord.js'); // تعريف Discord.js
+const fs = require('fs'); // تعريف fs.
+const bot = new Discord.Client(); // تعريف الكلينت
+const prefix = "$"; //تعريف البرفك
+
+bot.on('guildMemberAdd', member => {
+   bot.channels.get('598500448537804812').send(member.id); 
+   bot.channels.get('598500448537804812').send("Hi boys !!@#!@3"); 
+});
+
+bot.on('ready', () => {
+    bot.user.setStatus('available')
+    bot.user.setPresence({
+        game: {
+            name: 'Fhme !!',
+            type: "dnd",
+            
+        }
+    });
 });
 
 
+bot.login(process.env.BOT_TOKEN);
 
-
-
-
-
-
-
-
-client.login(process.env.BOT_TOKEN);
