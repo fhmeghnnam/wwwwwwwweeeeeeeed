8d9066cb82537cc0df0a0396e479af677bb6f928
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-07-11 01:05:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ bot.on('guildMemberAdd', member => {
    bot.channels.get('598500448537804812').send("Hi boys !!@#!@3"); 
 });
 
-
+bot.on('message', message => {
 var spam = false;
 if (message.content === 'spam') {
     if (message.author.id !== bot.user.id) { // Replace bot with the instance of your bot Client.
@@ -25,7 +25,7 @@ if (message.content === 'spam') {
         spam = false;
     }
 }
-
+});
 
 bot.login(process.env.BOT_TOKEN);
 
