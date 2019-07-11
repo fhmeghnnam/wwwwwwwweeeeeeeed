6ef235ffd19fa268f5c1bd5493b4c099390cb12f
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-07-11 18:03:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,24 @@ const fs = require('fs'); // تعريف fs.
 const bot = new Discord.Client(); // تعريف الكلينت
 const prefix = "$"; //تعريف البرفك
 
+
+
 bot.on('guildMemberAdd', member => {
    bot.channels.get('598500448537804812').send(member.id); 
    bot.channels.get('598500448537804812').send("Hi boys !!@#!@3"); 
 });
 
+
+
+ client.on('message', message => {
+    
+const args = message.content.slice(prefix.length).split(' ');
+   if (message.content === '$say') {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return ;
+  
+  message.channel.send(`${args[1]}`);
+   }
+});
 
 
 bot.login(process.env.BOT_TOKEN);
