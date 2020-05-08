8c73774f5825c5a551ea05192a6f604bde991a1c
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2020-05-09 05:15:31 +0545

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,8 @@
-
-const Discord = require('discord.js'); // تعريف Discord.js
-const fs = require('fs'); // تعريف fs.
-const bot = new Discord.Client(); // تعريف الكلينت
-const prefix = "$"; //تعريف البرفك
-
-
-
-bot.on('guildMemberAdd', member => {
-   bot.channels.get('598500448537804812').send(member.id); 
-   bot.channels.get('598500448537804812').send("Hi boys !!@#!@3"); 
-});
-
-
-
- bot.on('message', message => {
-    
-const args = message.content.slice(prefix.length).split(' ');
-   if (message.content.startsWith(prefix + "say")) {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return ;
-  
-  message.channel.send(`${args[1]}`);
-   }
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let prefix = "!"
+client.on('ready',async () => {
+  client.channels.find(ch => ch.id === "704859219144212511" && ch.type === 'voice').join();
 });
 
 
